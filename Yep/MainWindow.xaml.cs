e71146390345feef713358b33ad5cac732ed2ce6
fiveoclockgomie <ultimatetechieman@gmail.com>
2015-05-07 19:33:47 -0700

    no

---
+++ Yep/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using NetFwTypeLib;
+using System.Diagnostics;
+
+namespace Yep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int dank = 0;
+        string blockedEXE;
+        private void deleteRule()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "netsh.exe";
+            p.StartInfo.Arguments = string.Format("advfirewall firewall delete rule name=\"{0}\"", blockedEXE);
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Executable Files (*.exe)|*.exe";
+            if (openFileDialog.ShowDialog() == true)
+                pathBox.Text = openFileDialog.FileName;
+            blockedEXE = openFileDialog.FileName;
+        }
+
+        private void blockButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(blockedEXE) != true)
+            {
+                if (dank == 0)
+                {
+                    INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
+                        Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    List<INetFwRule> RuleList = new List<INetFwRule>();
+                    foreach (INetFwRule rule in firewallPolicy.Rules)
+                        RuleList.Add(rule);
+                    INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                    firewallRule.Description = String.Format("Block {0}", blockedEXE);
+                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                    firewallRule.ApplicationName = @blockedEXE;
+                    firewallRule.Enabled = true;
+                    firewallRule.InterfaceTypes = "All";
+                    firewallRule.Name = blockedEXE;
+
+                    firewallPolicy.Rules.Add(firewallRule);
+                    dank = 1;
+                }
+
+            }
+        }
+
+        private void unblockButton_Click(object sender, RoutedEventArgs e)
+        {
+            deleteRule();
+            dank = 0;
+        }
+    }
+}
