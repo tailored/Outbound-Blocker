07ed0df07256d6c4fc568d490d826a8da7195e54
fiveoclockgomie <ultimatetechieman@gmail.com>
2015-05-08 19:25:16 -0700

    Removed logic from button press event handlers

--- Outbound Blocker/MainWindow.xaml.cs
+++ Outbound Blocker/MainWindow.xaml.cs
@@ -26,17 +26,40 @@ namespace Yep
     {
         int createdRules = 0;
         string blockedEXE;
-        private void deleteRule()
+        private void deleteRule(string rulename)
         {
             Process p = new Process();
             p.StartInfo.FileName = "netsh.exe";
-            p.StartInfo.Arguments = string.Format("advfirewall firewall delete rule name=\"{0}\"", blockedEXE);
+            p.StartInfo.Arguments = string.Format("advfirewall firewall delete rule name=\"{0}\"", rulename);
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.CreateNoWindow = true;
             p.Start();
             // I opted to use netsh for the deletion of the rule because it allows for deletion of rules with simply their names, which the program controls
             //(and I failed to find documentation regarding the deletion of rules with the Windows Firewall API)
+        }
+        private void createRule(string appPath)
+        {
+            if (string.IsNullOrEmpty(appPath) != true)
+            {
+                if (createdRules == 0)
+                {
+                    INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
+                        Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                    firewallRule.Description = String.Format("Block {0}", appPath);
+                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                    firewallRule.ApplicationName = @appPath;
+                    firewallRule.Enabled = true;
+                    firewallRule.InterfaceTypes = "All";
+                    firewallRule.Name = appPath;
+                    // I opted to use the Windows Firewall API for creating the rule because it allows one to create (unattractively named, though easily identifiable) rules without
+                    // requiring information beyond the location of the program that needs its outbound connection blocked, which is retrieved from the OpenFileDialog and assigned to blockedEXE
+                    firewallPolicy.Rules.Add(firewallRule);
+                    createdRules = 1;
+                }
+            }
         }
         public MainWindow()
         {
@@ -49,42 +72,23 @@ namespace Yep
             openFileDialog.Filter = "Executable Files (*.exe)|*.exe";
             if (openFileDialog.ShowDialog() == true)
                 selectedPath.Text = openFileDialog.FileName;
-            blockedEXE = openFileDialog.FileName;
+            blockedEXE = selectedPath.Text;
         }
 
         private void blockButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(blockedEXE) != true)
-            {
-                if (createdRules == 0)
-                {
-                    INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
-                        Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
-                    List<INetFwRule> RuleList = new List<INetFwRule>();
-                    foreach (INetFwRule rule in firewallPolicy.Rules)
-                        RuleList.Add(rule);
-                    INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
-                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
-                    firewallRule.Description = String.Format("Block {0}", blockedEXE);
-                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
-                    firewallRule.ApplicationName = @blockedEXE;
-                    firewallRule.Enabled = true;
-                    firewallRule.InterfaceTypes = "All";
-                    firewallRule.Name = blockedEXE;
-                    // I opted to use the Windows Firewall API for creating the rule because it allows one to create (unattractively named, though easily identifiable) rules without
-                    // requiring information beyond the location of the program that needs its outbound connection blocked, which is retrieved from the OpenFileDialog and assigned to blockedEXE
-
-                    firewallPolicy.Rules.Add(firewallRule);
-                    createdRules = 1;
-                }
-
-            }
+            createRule(blockedEXE);    
         }
 
         private void unblockButton_Click(object sender, RoutedEventArgs e)
         {
-            deleteRule();
+            deleteRule(blockedEXE);
             createdRules = 0;
+        }
+
+        private void selectedPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            blockedEXE = selectedPath.Text;
         }
     }
 }
