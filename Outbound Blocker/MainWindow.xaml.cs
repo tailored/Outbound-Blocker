5cd939d1ee24c6cc484f4ca3265e0733c1768749
5OClockCharlie <UltimateTechieMan@gmail.com>
2015-05-11 10:57:23 -0700

    Update MainWindow.xaml.cs
    Opened the createRule(appPath) method to future usage by abstracting away specific references

--- Outbound Blocker/MainWindow.xaml.cs
+++ Outbound Blocker/MainWindow.xaml.cs
@@ -42,23 +42,20 @@ namespace Yep
         {
             if (string.IsNullOrEmpty(appPath) != true)
             {
-                if (createdRules == 0)
-                {
-                    INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
-                        Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
-                    INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
-                    firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
-                    firewallRule.Description = String.Format("Block {0}", appPath);
-                    firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
-                    firewallRule.ApplicationName = @appPath;
-                    firewallRule.Enabled = true;
-                    firewallRule.InterfaceTypes = "All";
-                    firewallRule.Name = appPath;
-                    // I opted to use the Windows Firewall API for creating the rule because it allows one to create (unattractively named, though easily identifiable) rules without
-                    // requiring information beyond the location of the program that needs its outbound connection blocked, which is retrieved from the OpenFileDialog and assigned to blockedEXE
-                    firewallPolicy.Rules.Add(firewallRule);
-                    createdRules = 1;
-                }
+
+                INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
+                    Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+                firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                firewallRule.Description = String.Format("Block {0}", appPath);
+                firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                firewallRule.ApplicationName = @appPath;
+                firewallRule.Enabled = true;
+                firewallRule.InterfaceTypes = "All";
+                firewallRule.Name = appPath;
+                // I opted to use the Windows Firewall API for creating the rule because it allows one to create (unattractively named, though easily identifiable) rules without
+                // requiring information beyond the location of the program that needs its outbound connection blocked, which is retrieved from the OpenFileDialog and assigned to blockedEXE
+                firewallPolicy.Rules.Add(firewallRule);
             }
         }
         public MainWindow()
@@ -77,7 +74,11 @@ namespace Yep
 
         private void blockButton_Click(object sender, RoutedEventArgs e)
         {
-            createRule(blockedEXE);    
+            if (createdRules == 0)
+            {
+                createRule(blockedEXE);  
+                createdRules = 1;
+            }
         }
 
         private void unblockButton_Click(object sender, RoutedEventArgs e)
